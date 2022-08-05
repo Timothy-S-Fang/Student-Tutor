623e0612d493a55e3febe3872203ae81915ad890
Isaaczhu9877 <opisaac24@gmail.com>
2022-08-05 15:04:08 -0700

    added create table statements

--- team21/SQL/scripts/createData.sql
+++ team21/SQL/scripts/createData.sql
@@ -1,20 +1,20 @@
 
 CREATE TABLE k_12 (
-    SID int PRIMARY KEY,
-    Age int NOT NULL
-    Name char(50),
+    StudentID int PRIMARY KEY,
+    Age int NOT NULL,
+    StudentName char(50),
     Exams char(30),
     UniApplication bit,
     SAT bit,
     STS int NOT NULL,
-    TutorID NOT NULL,
+    TutorID int NOT NULL,
     FOREIGN KEY (STS) REFERENCES Availabilities,
     FOREIGN KEY (TutorID) REFERENCES Tutors
 );
 
 CREATE TABLE University (
-    SID int PRIMARY KEY,
-    Name char(50),
+    StudentID int PRIMARY KEY,
+    StudentName char(50),
     Age int,
     LSAT bit,
     MCAT bit,
@@ -28,16 +28,16 @@ CREATE TABLE University (
 CREATE TABLE Availabilities (
     STS int PRIMARY KEY,
     MeetTimes char(100),
-    SID int NOT NULL,
+    StudentID int NOT NULL,
     TID int NOT NULL,
-    FOREIGN KEY (SID) REFERENCES k_12,
-    FOREIGN KEY (SID) REFERENCES University,
+    FOREIGN KEY (StudentID) REFERENCES k_12,
+    FOREIGN KEY (StudentID) REFERENCES University,
     FOREIGN KEY (TID) REFERENCES Tutors
 );
 
 CREATE TABLE Tutors (
     TutorID int PRIMARY KEY,
-    Name char(50),
+    StudentName char(50),
     tAge int,
     Ratings int,
     SubjectName char(50) NOT NULL,
@@ -46,18 +46,89 @@ CREATE TABLE Tutors (
 );
 
 CREATE TABLE NeedHelp (
-    SID integer,
+    StudentID integer,
     SubjectName Char(50)
-    PRIMARY KEY (SID, SubjectName),
-    FOREIGN KEY(SID) REFERENCES k_12,
-    FOREIGN KEY(SID) REFERENCES University
+    PRIMARY KEY (StudentID, SubjectName),
+    FOREIGN KEY(StudentID) REFERENCES k_12,
+    FOREIGN KEY(StudentID) REFERENCES University
 );
 
 
+CREATE TABLE CanTeach(
+    SubjectName char(50),
+    TutorID int,
+    PRIMARY KEY (SubjectName, TutorID),
+    FOREIGN KEY (SubjectName) REFERENCES Subject,
+    FOREIGN KEY (TutorID) REFERENCES Tutor
+)
 
+CREATE TABLE Reports(
+    ReportNumber int PRIMARY KEY,
+    ReportDesc char(1000)
+)
+
+CREATE TABLE WriteReport(
+    ReportNumber int,
+    TutorID int,
+    PRIMARY KEY(ReportNumber, TutorID),
+    FOREIGN KEY(ReportNumber) REFERENCES Reports,
+    FOREIGN KEY(TutorID) REFERENCES Tutors
+)
+
+CREATE TABLE ReceiveReport(
+    ReportNumber int,
+    StudentID int,
+    PRIMARY KEY(ReportNumber, StudentID),
+    FOREIGN KEY(ReportNumber) REFERENCES Reports,
+    FOREIGN KEY(StudentID) REFERENCES Student
+)
+
+CREATE TABLE Subjects(
+    SubjectName char(20) PRIMARY KEY
+)
+
+CREATE TABLE Courses(
+    CourseName char(50) PRIMARY KEY,
+    GradeLevel int,
+    SubjectName char(80) NOT NULL,
+    FOREIGN KEY(SubjectName) REFERENCES Subjects
+)
+
+CREATE TABLE Topics(
+    TopicName char(20),
+    CourseName char(20),
+    Difficult int,
+    PRIMARY KEY(TopicName, CourseName),
+    FOREIGN KEY(TopicName) REFERENCES Topics
+)
+
+CREATE TABLE Assignment(
+    AssignNumber int PRIMARY KEY,
+    AssignDescription char(1000),
+    Mark int
+)
+
+CREATE TABLE Give(
+    AssignNumber int,
+    StudentID int,
+    PRIMARY KEY(AssignNumber, StudentID),
+    FOREIGN KEY(AssignNumber) REFERENCES Assignment,
+    FOREIGN KEY(StudentID) REFERENCES K_12,
+    FOREIGN KEY(StudentID) REFERENCES University
+)
+
+CREATE TABLE Has(
+    TopicName char(20),
+    TutorID integer,
+    AssignNumber integer,
+    PRIMARY KEY(TopicName, TutorID, AssignNumber),
+    FOREIGN KEY(TopicName, CourseName) REFERENCES Topic,
+    FOREIGN KEY(TutorID) REFERENCES Tutor,
+    FOREIGN KEY(AssignNumber) REFERENCES Assignment
+)
 
 INSERT INTO k_12 VALUES(35694356,"Tim", 18, 1,	0, 0, 112312, 1)
-    INSERT INTO k_12 VALUES(24357532,"Jimmy", 16, 0, 1, 1, 2, 1)
+INSERT INTO k_12 VALUES(24357532,"Jimmy", 16, 0, 1, 1, 2, 1)
 INSERT INTO k_12 VALUES(13294586,"Bob,", 14, 0, 0,	0, 184935, 4)
 INSERT INTO k_12 VALUES(19283844,"Jones", 12,	0, 0, 0, 192383, 5)
 INSERT INTO k_12 VALUES(10293948,"Johnny,", 18, 1,	1, 1, 123944, 6)
