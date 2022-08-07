990a3633c5b7b3a88d6c2b6f1476d3ea05bc1e12
SeanMiffuine <work.seantang@gmail.com>
2022-08-06 21:58:36 -0700

    Added php scripts for student/tutor login and signup

---
+++ team21/SQL/scripts/studentLogin.php
@@ -0,0 +1,13 @@
+html>
+    <body>
+    <head>
+        <title>LOGIN</title>
+    </head>
+
+
+    <form method="POST" action="login.php">
+        <p><input type="submit" value="login" name="login"></p>
+    </form>
+
+</body>
+</html>
